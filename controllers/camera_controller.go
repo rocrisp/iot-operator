c4ab71b6a9c65ec1d9ff65efcfde2607b67c5aa5
Rose Crisp <rocrisp@redhat.com>
2023-04-08 19:08:04 -0400

    add rbac rules

--- controllers/camera_controller.go
+++ controllers/camera_controller.go
@@ -51,6 +51,9 @@ type CameraReconciler struct {
 //+kubebuilder:rbac:groups=mygroup.iot.com,resources=cameras,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=mygroup.iot.com,resources=cameras/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=mygroup.iot.com,resources=cameras/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=events,verbs=create;patch
+//+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
--- controllers/sensor_controller.go
+++ controllers/sensor_controller.go
@@ -51,6 +51,9 @@ type SensorReconciler struct {
 //+kubebuilder:rbac:groups=mygroup.iot.com,resources=sensors,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=mygroup.iot.com,resources=sensors/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=mygroup.iot.com,resources=sensors/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=events,verbs=create;patch
+//+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
